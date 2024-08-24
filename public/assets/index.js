1696d24163e9a8dda07eb562ac17d7c2070f3795
mr-olabi <your-email@example.com>
2024-08-24 21:46:18 +0300

    rating page added for desktop screens

--- public/assets/index.js
+++ public/assets/index.js
@@ -5,6 +5,10 @@ import ChairCategoryImg from '../assets/chair2.png'
 import Sofa1CarouselImg from '../assets/sofa2.jpg'
 import Sofa2CarouselImg from '../assets/sofa.png'
 import Sofa3CarouselImg from '../assets/chair1.png'
+import RatingPerson1Img from '../assets/joeadams.png'
+import RatingPerson2Img from '../assets/vladapetrov.png'
+import RatingPerson3Img from '../assets/sarahamary.png'
+import RatingPerson4Img from '../assets/taimhosni.png'
 
 export {
     LuxuryChairImg,
@@ -13,6 +17,10 @@ export {
     Sofa3CarouselImg,
     ChairCategoryImg,
     SofaCategoryImg,
-    Sofa2CategoryImg
+    Sofa2CategoryImg,
+    RatingPerson1Img,
+    RatingPerson2Img,
+    RatingPerson3Img,
+    RatingPerson4Img
 
 }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,109 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { 
+  RatingPerson1Img, 
+  RatingPerson2Img, 
+  RatingPerson3Img, 
+  RatingPerson4Img 
+} from '../../public/assets'
 
 const Rating = () => {
   return (
-    <div id='rating'>
-      
+    <div id='rating' className='ml-16 mt-[15rem] mb-[7rem] py-7'>
+      <h3 className='text-2xl text-[#2f2f2f] font-semibold'>
+        Popular Rates
+      </h3>
+
+      {/* Rating Boxes */}
+      <div className='mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4'>
+       
+
+        <div className=' p-1 w-[145px] rounded-xl'>
+          <img 
+          src={RatingPerson1Img}
+          className='border-[5px] border-[#ffffff] rounded-xl shadow-lg'
+          alt="rating person img Joe Adams" 
+          />
+          <h3 className='text-[#2f2f2f] font-bold text-xl mt-2 text-center'>
+          Joe Adams
+        </h3> 
+
+        <p className='text-xs font-regular text-[#2f2f2f] mt-2 text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+
+          </p> 
+
+          <div className='mt-2 flex items-center text-[#2f2f2f]'>
+            <FaStar size={35}/>            <FaStar size={35}/>            <FaStar size={35}/>            <FaStar size={35}/>            <FaStar size={35}/>
+            </div>
+        </div>
+        
+        
+        <div className='p-1 w-[145px] rounded-xl'>
+          <img 
+          src={RatingPerson2Img} 
+          className='border-[5px] border-[#ffffff] rounded-xl shadow-lg'
+          alt="rating person img Vlada Petrov" 
+          />
+           <h3 className='text-[#2f2f2f] font-bold text-xl mt-2 text-center'>
+          Vlada Petrov
+        </h3>
+
+        <p className='text-xs font-regular text-[#2f2f2f] mt-2 text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+
+        </p>
+
+        <div className='mt-2 flex items-center text-[#2f2f2f]'>
+          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStarHalfAlt size={35}/>
+        </div>
+        </div>
+        
+       
+        <div className='p-1 w-[145px] rounded-xl'>
+          <img 
+          src={RatingPerson3Img} 
+          className='border-[5px] border-[#ffffff] rounded-xl shadow-lg'
+          alt="rating person img Sarah Amary" 
+          />
+             <h3 className='text-[#2f2f2f] font-bold text-xl mt-2 text-center'>
+          Sarah Amary
+        </h3>
+
+        <p className='text-xs font-regular text-[#2f2f2f] mt-2 text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+        </p>
+
+        <div className='mt-2 flex items-center text-[#2f2f2f]'>
+          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>
+        </div>
+        </div>
+        
+     
+        <div className='p-1 w-[145px] rounded-xl'>
+          <img 
+          src={RatingPerson4Img}
+          className='border-[5px] border-[#ffffff] rounded-xl shadow-lg'
+          alt="Taim Hosni" 
+          />
+        <h3 className='text-[#2f2f2f] font-bold text-xl mt-2 text-center'>
+          Taim Hosni
+        </h3>
+
+        <p className='text-xs font-regular text-[#2f2f2f] mt-2 text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+         
+        </p>
+
+        <div className='mt-2 flex items-center text-[#2f2f2f]'>
+          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStar size={35}/>          <FaStarHalfAlt size={35}/>
+        </div>
+        </div>
+
+      </div>
     </div>
   )
 }
 
-export default Rating
+export default Rating;
